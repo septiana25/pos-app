e0b8ae9e4046e6d1536c2c88833717ace7198a75
septiana25 <insaidseptiana@gmail.com>
2025-08-14 23:27:00 +0700

    WIP: Kategori set route grouping

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,6 +16,14 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {    
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    //name route: mater-data.kategori.index
+    //slug routemaster-data/kategori/index
+    Route::prefix('master-data')->as('master-data.')->group(function (){
+        Route::prefix('kategori')->as('kategori.')->controller(KategoriController::class)->group(function (){
+            Route::get('/', 'index')->name('index');
+        });
+    });
 
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
